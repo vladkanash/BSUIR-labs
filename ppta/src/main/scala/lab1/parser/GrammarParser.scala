566fb7dee936ded34f1ad00f332e128b69a5e58f
Vlad Kanash <vlad.kanash@expert-soft.by>
2017-12-10 12:43:08 +0300

    GrammarParser finished.

--- ppta/src/main/scala/lab1/parser/GrammarParser.scala
+++ ppta/src/main/scala/lab1/parser/GrammarParser.scala
@@ -10,7 +10,7 @@ import scala.util.parsing.combinator.RegexParsers
 object GrammarParser extends RegexParsers with RuleParser {
 
   val ruleSep = ";"
-  val startSymbolRegexp = "^[a-zA-Z]$".r
+  val startSymbolRegexp: Regex = "^[a-zA-Z]$".r
   val rulesStart = "Rules: "
   val terminalsStart = "Terminals: "
   val nonTerminalsStart = "NonTerminals: "
@@ -19,14 +19,19 @@ object GrammarParser extends RegexParsers with RuleParser {
 
   override val whiteSpace: Regex = """[ \t]+""".r
 
-  def grammar: Parser[Grammar] = (terminals <~ eol) ~ (nonTerminals <~ eol) ~ (rules <~ eol) ~ (startSymbol <~ eol) ^^
-    { case ((terms ~ nonTerms) ~ rls) ~ start => new Grammar(terms.toSet, nonTerms.toSet, rls.toSet, start) }
+  def grammar: Parser[Grammar] = (terminals <~ eol) ~ (nonTerminals <~ eol) ~ (rules <~ eol) ~ (startSymbol <~ eol) ^^ {
+    case ((terms ~ nonTerms) ~ rls) ~ start => new Grammar(terms.toSet, nonTerms.toSet, rls.toSet, start)
+  }
 
-  def rules: Parser[List[Rule]] = rulesStart ~> rule ~ rep(ruleSep ~> rule) <~ opt(ruleSep) ^^ (e => e._1 ::: e._2.flatten)
-  def terminals: Parser[List[Symbol]] = terminalsStart ~> seq
-  def nonTerminals: Parser[List[Symbol]] = nonTerminalsStart ~> seq
+  def rules: Parser[List[Rule]] = rulesStart ~> rule ~ rep(ruleSep ~> rule) <~ opt(ruleSep) ^^
+    (e => e._1 ::: e._2.flatten)
+
   def startSymbol: Parser[Symbol] = startSymbolStart ~> startSymbolRegexp ^^
     (res => Symbol(res.charAt(0)))
+
+  def terminals: Parser[List[Symbol]] = terminalsStart ~> seq
+
+  def nonTerminals: Parser[List[Symbol]] = nonTerminalsStart ~> seq
 
   def seq: Parser[List[Symbol]] = nonEmptyWord ^^ (_.toList.map(Symbol(_)))
 
--- ppta/src/main/scala/lab1/parser/RuleParser.scala
+++ ppta/src/main/scala/lab1/parser/RuleParser.scala
@@ -2,17 +2,20 @@ package lab1.parser
 
 import lab1.{Rule, Word}
 
+import scala.util.matching.Regex
 import scala.util.parsing.combinator.RegexParsers
 
 trait RuleParser extends RegexParsers {
 
   val sep = "|"
   val arrow = "=>"
-  val emptyWord = "E"
-  val nonEmptyWord = "[a-zA-Z]+".r
+  val emptyWord = "ℰ"
+  val nonEmptyWord: Regex = "[a-zA-Z]+".r
 
   def rule: Parser[List[Rule]] = (word <~ arrow) ~ right ^^ (res => res._2.map(right => Rule(res._1, right)))
+
   def right: Parser[List[Word]] = word ~ rep(sep ~> word) ^^ (e => e._1 :: e._2)
+
   def word: Parser[Word] = emptyWord ^^ (_ => Word.empty) | nonEmptyWord ^^ (Word(_))
 
   def parse(str: CharSequence): List[Rule] = parseAll(rule, str) match {
