475ea05d2e3dde8c3b2aeb8d1d20ede5523928f3
Vlad Kanash <vlad.kanash@expert-soft.by>
2017-12-11 14:16:31 +0300

    DeterminateFiniteStateMachine completed.

--- ppta/src/main/scala/lab2/DeterminateFiniteStateMachine.scala
+++ ppta/src/main/scala/lab2/DeterminateFiniteStateMachine.scala
@@ -4,20 +4,17 @@ import lab1.{Grammar, Symbol}
 
 class DeterminateFiniteStateMachine(grammar: Grammar) extends FiniteStateMachine(grammar: Grammar) {
 
-  type TransitionsColumn = Map[Symbol, Set[State]]
-  type TransitionsTable = Map[Set[State], TransitionsColumn]
+  type TransitionsEntry = Map[Symbol, Set[State]]
+  type TransitionsMap = Map[Set[State], TransitionsEntry]
 
-  private val determinationTable: TransitionsTable = {
-    def getTransitionsColumn(dStates: Set[State]): TransitionsColumn =
+  private val deteminationMap: TransitionsMap = {
+    def getTransitionsColumn(dStates: Set[State]): TransitionsEntry =
       inputs.map(input => input -> dStates.flatMap(getReachableStates(_, input))).toMap
 
     def getReachableStates(state: State, input: Symbol) =
-      transitions
-        .filter(_.inputSymbol == input)
-        .filter(_.inputState == state)
-        .map(_.outputState)
+      transitions.filter(t => (t.inputSymbol == input) && (t.inputState == state)).map(_.outputState)
 
-    def createNewStates(processed: TransitionsTable, newStates: Set[Set[State]]): TransitionsTable = {
+    def createNewStates(processed: TransitionsMap, newStates: Set[Set[State]]): TransitionsMap = {
       val newColumns = newStates.map(state => state -> getTransitionsColumn(state)).toMap
       val filteredColumns = newColumns.filterNot(col => processed.contains(col._1))
       val createdStates = filteredColumns.values.flatten.map(_._2).filterNot(_.isEmpty).toSet
@@ -27,8 +24,24 @@ class DeterminateFiniteStateMachine(grammar: Grammar) extends FiniteStateMachine
     createNewStates(Map.empty, Set(Set(startState)))
   }
 
-  val determinateStates: Set[State] = ???
-  val determinateEndStates: Set[State] = determinateStates.filter(_ => false)
-  val determinateTransitions: Set[Transition] = ???
+  private val rawNewStates = deteminationMap.keySet.filter(_.size > 1)
+  private val newStateNames = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" diff states.mkString take rawNewStates.size
+  private val newStatesMap = rawNewStates.zip(newStateNames.map(State(_))).toMap.withDefault(_.head)
+
+  val determinateStates: Set[State] = states ++ rawNewStates.map(newStatesMap)
+
+  val determinateEndStates: Set[State] = endStates ++
+    rawNewStates.filterNot(s => (s & endStates).isEmpty).map(newStatesMap)
+
+  val determinateTransitions: Set[Transition] = deteminationMap.flatMap(entry =>
+      entry._2.filterNot(cell => cell._2.isEmpty)
+        .map(cell => Transition(newStatesMap(entry._1), cell._1, newStatesMap(cell._2)))).toSet
+
+//  override val toString: String =
+//    s"""Q (States): ${determinateStates.mkString}
+//       |T (Input Symbols): ${inputs.mkString}
+//       |F (Transitions): ${determinateTransitions.mkString("\n\t", ";\n\t", "")}
+//       |H (Start states): $startState
+//       |Z (End states): ${determinateEndStates.mkString}""".stripMargin
 
 }
