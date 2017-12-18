4093eaf34022689f0bf648820f4a21e3518c1def
Vlad Kanash <vlad.kanash@expert-soft.by>
2017-12-18 11:36:57 +0300

    Code cleanup.

--- ppta/src/main/scala/lab2/Deterministic.scala
+++ ppta/src/main/scala/lab2/Deterministic.scala
@@ -11,15 +11,27 @@ trait Deterministic extends FiniteStateMachine {
 
   private val determinationMap: TransitionsMap = {
     def getTransitionsColumn(dStates: Set[State]): TransitionsEntry =
-      inputs.map(input => input -> dStates.flatMap(getReachableStates(_, input))).toMap
+      inputs
+        .map(input => input -> dStates
+        .flatMap(getReachableStates(_, input))).toMap
 
     def getReachableStates(state: State, input: Symbol) =
-      super.transitions.filter(t => (t.inputSymbol == input) && (t.inputState == state)).map(_.outputState)
+      super.transitions
+        .filter(t => (t.inputSymbol == input) && (t.inputState == state))
+        .map(_.outputState)
 
     def createNewStates(processed: TransitionsMap, newStates: Set[Set[State]]): TransitionsMap = {
-      val newColumns = newStates.map(state => state -> getTransitionsColumn(state)).toMap
-      val filteredColumns = newColumns.filterNot(col => processed.contains(col._1))
-      val createdStates = filteredColumns.values.flatten.map(_._2).filterNot(_.isEmpty).toSet
+      val newColumns = newStates
+        .map(state => state -> getTransitionsColumn(state))
+        .toMap
+
+      val filteredColumns = newColumns
+        .filterNot(col => processed.contains(col._1))
+
+      val createdStates = filteredColumns
+        .values.flatten.map(_._2)
+        .filterNot(_.isEmpty).toSet
+
       if (filteredColumns.isEmpty) processed else createNewStates(processed ++ filteredColumns, createdStates)
     }
 
@@ -28,16 +40,20 @@ trait Deterministic extends FiniteStateMachine {
 
   private val rawNewStates = determinationMap.keySet.filter(_.size > 1)
 
-  private val newStatesMap = rawNewStates.zip(deterministicNewStateNames.map(State(_))).toMap.withDefault(_.head)
+  private val newStatesMap = rawNewStates
+    .zip(deterministicNewStateNames.map(State(_)))
+    .toMap.withDefault(_.head)
 
   override def newStateNames: String = deterministicNewStateNames
 
   override def states: Set[State] = super.states ++ rawNewStates.map(newStatesMap)
 
-  override def endStates: Set[State] = super.endStates ++
-    rawNewStates.filterNot(s => (s & super.endStates).isEmpty).map(newStatesMap)
+  override def endStates: Set[State] = rawNewStates
+    .filterNot(s => (s & super.endStates).isEmpty)
+    .map(newStatesMap) ++ super.endStates
 
-  override def transitions: Set[Transition] = determinationMap.flatMap(entry =>
-      entry._2.filterNot(cell => cell._2.isEmpty)
-        .map(cell => Transition(newStatesMap(entry._1), cell._1, newStatesMap(cell._2)))).toSet
+  override def transitions: Set[Transition] = determinationMap
+    .flatMap(entry => entry._2.filterNot(cell => cell._2.isEmpty)
+      .map(cell => Transition(newStatesMap(entry._1), cell._1, newStatesMap(cell._2))))
+    .toSet
 }
--- ppta/src/main/scala/lab3/Minimized.scala
+++ ppta/src/main/scala/lab3/Minimized.scala
@@ -9,33 +9,44 @@ trait Minimized extends FiniteStateMachine {
 
   private val reachableStates: Set[State] = {
 
-    def extendStates(reachedStates: Set[State], newStates: Set[State]): Set[State] = {
-      val reached = newStates.flatMap(state =>
-        super.transitions.filter(_.inputState == state).map(_.outputState)) ++ newStates -- reachedStates
-      if (reached.isEmpty) reachedStates else extendStates(reachedStates ++ reached, reached)
+    def reachStates(reachedStates: Set[State], newStates: Set[State]): Set[State] = {
+      val reached = newStates
+        .flatMap(state => super.transitions
+          .filter(_.inputState == state)
+          .map(_.outputState)) ++ newStates -- reachedStates
+      if (reached.isEmpty) reachedStates else reachStates(reachedStates ++ reached, reached)
     }
 
-    extendStates(Set.empty, Set(super.startState))
+    reachStates(Set.empty, Set(super.startState))
   }
 
   private val uniqueStatesMap: Map[State, State] = {
 
-    def getReachMap(state: State, classes: Array[Set[State]]): Map[Symbol, Int] = super.transitions
-      .filter(_.inputState == state)
-      .map(transition => transition.inputSymbol ->
-        classes.indexOf(classes.find(_.contains(transition.outputState)).get))
-      .toMap
+    def getReachMap(state: State, classes: Array[Set[State]]): Map[Symbol, Int] =
+      super.transitions
+        .filter(_.inputState == state)
+        .map(transition => transition.inputSymbol ->
+          classes.indexOf(classes.find(_.contains(transition.outputState)).get))
+        .toMap
 
     def extendClasses(classes: Set[Set[State]]): Set[Set[State]] = {
-      val newClasses = classes.flatten.groupBy((state: State) => getReachMap(state, classes.toArray)).values.toSet
+      val newClasses = classes.flatten
+        .groupBy((state: State) => getReachMap(state, classes.toArray))
+        .values.toSet
       if (classes == newClasses) newClasses else extendClasses(newClasses)
     }
 
-    val stateClasses = extendClasses(reachableStates.partition(super.endStates.contains)
-      .productIterator.map(_.asInstanceOf[Set[State]]).toSet)
+    val firstClassStates = reachableStates
+      .partition(super.endStates.contains)
+      .productIterator.map(_.asInstanceOf[Set[State]])
+      .toSet
 
-    stateClasses.filter(_.size > 1).zip(minimizedNewStateNames.map(State(_)))
-      .flatMap(states => states._1.map(s => (s, states._2)))
+    val stateClasses = extendClasses(firstClassStates)
+
+    stateClasses
+      .filter(_.size > 1)
+      .zip(minimizedNewStateNames.map(State(_)))
+      .flatMap(states => states._1.map(state => (state, states._2)))
       .toMap.withDefault(identity)
   }
 
