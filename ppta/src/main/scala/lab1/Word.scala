caf8e0266e23acfbaf24163afbd0844bae5f8ce4
vladkanash <vlad.kanash@gmail.com>
2017-12-25 15:11:01 +0300

    Code cleanup.

--- ppta/src/main/scala/lab1/Word.scala
+++ ppta/src/main/scala/lab1/Word.scala
@@ -3,11 +3,8 @@ package lab1
 sealed trait Word {
 
   def len: Int
-
   def contents: List[Symbol]
-
   def allIn(seq: Set[Symbol]): Boolean = (contents.toSet -- seq).isEmpty
-
   def hasAnyIn(seq: Set[Symbol]): Boolean = (contents.toSet & seq).nonEmpty
 }
 
@@ -16,9 +13,7 @@ case object EmptyWord extends Word {
   private val emptyWordRep = 'ε'
 
   override val len = 0
-
   override val contents: List[Symbol] = List.empty
-
   override val toString: String = emptyWordRep.toString
 }
 
@@ -26,9 +21,7 @@ case class SymbolicWord(value: CharSequence) extends Word {
   require(value.length() > 0, "word cannot be null")
 
   override val len: Int = value.length()
-
   override val contents: List[Symbol] = value.toString.map(Symbol(_)).toList
-
   override val toString: String = value.toString
 }
 
@@ -40,6 +33,7 @@ object Word {
 
   implicit def symbolSetToWord(set: Set[Symbol]): Word = Word(set.mkString)
   implicit def symbolListToWord(list: List[Symbol]): Word = Word(list.mkString)
+  implicit def stringToWord(str: String): Word = Word(str)
 }
 
 
